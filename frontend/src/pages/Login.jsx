38cd782a06deb7970eecb97cc6fe406933cde126
J-sankar <jayasankarmenonv@gmail.com>
2026-02-08 10:21:11 +0530

    remove unwanted testfile

--- frontend/src/pages/Login.jsx
+++
@@ -1,9 +0,0 @@
-import { signInWithGoogle } from "../lib/auth";
-
-export default function Login() {
-  return (
-    <button onClick={signInWithGoogle}>
-      Continue with Google
-    </button>
-  );
-}
